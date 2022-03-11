1258271c715e7a6fd98ef00c01793e49a34e6e88
benji694 <bbouillard@edenschool.fr>
2022-03-11 14:31:00 +0100

    create class name and funcion for every numbers

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
-console.log('hello');
+const nb1 = document.querySelector('#nb1');
+const nb2 = document.querySelector('#nb2');
+const nb3 = document.querySelector('#nb3');
+const nb4 = document.querySelector('#nb4');
+
+
+
+function n1(elt){
+    elt.firstElementChild.className += ' haut gauche bas';
+    elt.lastElementChild.className += ' haut gauche bas';
+}
+function n2(elt){
+    elt.firstElementChild.className += '  gauche';
+    elt.lastElementChild.className += ' droite';
+}
+function n3(elt){
+    elt.firstElementChild.className += ' gauche';
+    elt.lastElementChild.className += ' gauche';
+}
+function n4(elt){
+    elt.firstElementChild.className += ' haut';
+    elt.lastElementChild.className += '  gauche bas';
+}
+function n5(elt){
+    elt.firstElementChild.className += ' droite';
+    elt.lastElementChild.className += ' gauche';
+}
+function n6(elt){
+    elt.firstElementChild.className += ' droite';
+}
+function n7(elt){
+    elt.firstElementChild.className += ' gauche bas';
+    elt.lastElementChild.className += ' haut gauche bas';
+}
+
+function n9(elt){
+    elt.lastElementChild.className += ' gauche';
+}
+
+n9(nb1)
